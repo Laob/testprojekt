b14ee3f18b06893c4c74174ce29c8e05d2cdf6ec
DM_Dozent <dozent@dm.dm>
2016-02-23 09:36:00 +0100

    Erster Commit

---
+++ Testprojekt/src/Welt.java
@@ -0,0 +1,22 @@
+
+public class Welt {
+
+	public static void main(String[] args) {
+		int i=1; 
+		int j=2;
+		int temp = 0;
+
+		while(i<=20) {
+			if(i==(j+temp)) {
+				System.out.println(-i);
+				temp = j+temp;
+				j++;
+			} else {
+				System.out.println(i);
+			}
+			i++;
+		}
+
+	}
+
+}
